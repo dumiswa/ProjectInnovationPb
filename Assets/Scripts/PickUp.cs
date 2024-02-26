e43c0cc57c04d91ecbba73f53298e03172635f2c
dumiswa <dumitrudumi605@yahoo.com>
2024-02-26 13:33:59 +0100

    PickUp Script

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private bool hasAnItem;
+    private bool canPickUp;
+
+    public GameObject ownedItemInstance;
+    public GameObject ownedItemPrefab;
+    private void Start()
+    {
+        hasAnItem = false;
+        canPickUp = true;
+    }
+
+    private void Update()
+    {
+_       if (canPickUp && hasAnItem!) 
+        {
+            
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("BlackHoles") && ownedItemInstance == null)
+        {
+            canPickUp = false;
+
+            ownedItemPrefab = other.gameObject;
+
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void update()
+    {
+        if (Input.GetMouseButtonDown(0) && canPickUp! && ownedItemPrefab != null) 
+        {
+            ownedItemInstance = Instantiate(ownedItemPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+
+}
