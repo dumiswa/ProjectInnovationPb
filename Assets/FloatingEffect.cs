167b37cbf1a36bfc66a5d1c189ea15f6eed993df
Ana Similiac <53095@student.saxion.nl>
2024-03-07 15:48:22 +0100

    In game menu+ leader board menu

---
+++ Assets/FloatingEffect.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class FloatingEffect : MonoBehaviour
+{
+    public float amplitude = 0.5f; // Height of the float effect
+    public float frequency = 1f; // Speed of the float effect
+
+    // Position Storage Variables
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+    // Use this for initialization
+    void Start()
+    {
+        // Store the starting position & rotation of the object
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Float up/down with a Sin()
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+    }
+}
